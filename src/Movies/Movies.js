c9b32477900a42cce39c0e1dca78fa4090a3ac1f
meghana <meghanamaggie28@gmail.com>
2021-01-20 18:36:07 +0530

    changed as 13

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -3,28 +3,28 @@ import Movie from '../Movie/Movie.js';
 import './styles.css';
 
 class Movies extends React.Component {
+    getMovie = (name,likes,banner) => {
+        return <Movie name={name} likes={likes} banner={banner} />;
+    };
     render() {
         return (
         <div className='movies-section'>
-          <Movie
-          name='Maharshi'
-          likes='123.0k' 
-          banner='https://th.bing.com/th/id/OIP.dEyezZftuInXU_j0TDXPIAHaKX?pid=Api&rs=1'
-          hero='mahesh' />
+            {this.getMovie(
+                'Maharshi' ,
+                '123.0k' ,
+                'https://th.bing.com/th/id/OIP.dEyezZftuInXU_j0TDXPIAHaKX?pid=Api&rs=1' )}
           
-
-         
-          <Movie
-          name='Avengersendgame'
-          likes='1m' 
-          banner='https://th.bing.com/th/id/OIP.QS9ONR943YPNFP0TcFpUNgHaK0?pid=Api&rs=1'
-         />
-
-         <Movie
-          name='jersey'
-          likes='180k' 
-          banner='https://1.bp.blogspot.com/-oaxG0yYDGyw/XMGCscI9S6I/AAAAAAAAAKo/-1R9WjONUYQPqi6-7EkH1auhx16HmCDogCLcBGAs/s1600/D4hTGPjU4AMmVCS.jpg'
-         />
+          {this.getMovie(
+                'vengersendgame' ,
+                '1m' ,
+                'https://th.bing.com/th/id/OIP.QS9ONR943YPNFP0TcFpUNgHaK0?pid=Api&rs=1' )}
+          
+          {this.getMovie(
+                'jersey' ,
+                '180k' ,
+                ' https://1.bp.blogspot.com/-oaxG0yYDGyw/XMGCscI9S6I/AAAAAAAAAKo/-1R9WjONUYQPqi6-7EkH1auhx16HmCDogCLcBGAs/s1600/D4hTGPjU4AMmVCS.jpg'
+                )}
+          
 </div>
 
 
