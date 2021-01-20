a800da6109c507837dd994041db058754de4cef4
meghana <meghanamaggie28@gmail.com>
2021-01-20 16:37:01 +0530

    changed as 10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Movie from './Movie/Movie';
+import Movies from './Movies/Movies';
 
 function App() {
   return (
     <div className="App">
          <h1>Hai</h1>
-         <Movie name='Maharshi' likes='123.0k' banner='https://th.bing.com/th/id/OIP.dEyezZftuInXU_j0TDXPIAHaKX?pid=Api&rs=1' hero='mahesh' />
+         <Movies />
+         <Movie name='Maharshi'
+          likes='123.0k' 
+          banner='https://th.bing.com/th/id/OIP.dEyezZftuInXU_j0TDXPIAHaKX?pid=Api&rs=1'
+          hero='mahesh' />
          
     </div>
   );
---
+++ src/Movies/Movies.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Movies extends React.Component {
+    render() {
+        return <div>Movies</div>
+    }
+}
+
+
+export default Movies;
