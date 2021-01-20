abb1747ab73f7dc1d80e15f3e3909596626abfb7
meghana <meghanamaggie28@gmail.com>
2021-01-20 18:06:04 +0530

    changed as 12

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,13 +1,19 @@
-import * as react from 'react';
+import React from 'react';
 import './style.css';
-function Movie(props) {
-    return (
-    <div className='movie-card'>
-        <img src={props.banner}/>
-        <label>
-            {props.likes} {props.name} {props.hero}
-        </label>
-    </div>
-    );
+
+class Movie extends React.Component {
+    render() {
+        return (
+            <div className='movie-card'>
+                <img src={this.props.banner}/>
+                <label>
+                    {this.props.likes} {this.props.name} {this.props.hero}
+                </label>
+            </div>
+
+        );
+
+    }
 }
+
     export default Movie;
