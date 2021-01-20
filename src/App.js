974ffc09c5734a872f985c3a21d9521cf15b31b2
meghana <meghanamaggie28@gmail.com>
2021-01-20 17:46:50 +0530

    changed as 11

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Movie from './Movie/Movie';
-import Movies from './Movies/Movies';
+import Movies from './Movies/Movies.js';
 
 function App() {
   return (
     <div className="App">
          <h1>Hai</h1>
          <Movies />
-         <Movie name='Maharshi'
-          likes='123.0k' 
-          banner='https://th.bing.com/th/id/OIP.dEyezZftuInXU_j0TDXPIAHaKX?pid=Api&rs=1'
-          hero='mahesh' />
          
     </div>
   );
--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import Movie from '../Movie/Movie.js';
+import './styles.css';
 
 class Movies extends React.Component {
     render() {
-        return <div>Movies</div>
+        return (
+        <div className='movies-section'>
+          <Movie
+          name='Maharshi'
+          likes='123.0k' 
+          banner='https://th.bing.com/th/id/OIP.dEyezZftuInXU_j0TDXPIAHaKX?pid=Api&rs=1'
+          hero='mahesh' />
+          
+
+         
+          <Movie
+          name='Avengersendgame'
+          likes='1m' 
+          banner='https://th.bing.com/th/id/OIP.QS9ONR943YPNFP0TcFpUNgHaK0?pid=Api&rs=1'
+         />
+
+         <Movie
+          name='jersey'
+          likes='180k' 
+          banner='https://1.bp.blogspot.com/-oaxG0yYDGyw/XMGCscI9S6I/AAAAAAAAAKo/-1R9WjONUYQPqi6-7EkH1auhx16HmCDogCLcBGAs/s1600/D4hTGPjU4AMmVCS.jpg'
+         />
+</div>
+
+
+
+
+
+        );   
     }
 }
 
